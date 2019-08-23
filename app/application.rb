15c0579536c423a5db421772210f55a80f070cb2
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-23 02:03:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,13 @@ class Application
         end
       end
     elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
