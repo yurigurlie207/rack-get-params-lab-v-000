f9ee81ce3500e5e4efacc5525f55ee704c92eac0
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-23 02:01:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,13 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
     elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
